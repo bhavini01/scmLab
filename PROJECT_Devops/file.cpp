f192d9a5359ec837a9161bd89684d362074b512a
Bhavini Mishra <mily.mishra01@gmail.com>
2019-04-17 23:44:42 +0530

    header files

---
+++ PROJECT_Devops/file.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
+#include<fstream>
+#include<cstring>
+#include<cstdio>
+#include<cstdlib>
+#include<iomanip>
+using namespace std;
+//global variable declaration
+int k=7,r=0,flag=0;
+COORD coord = {0, 0};
+
